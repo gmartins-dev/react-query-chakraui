38fbb719e04ef4e23261dbf22fe58509ca09613b
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-06-08 08:52:02 -0300

    projeto reininciado, reset geral, chakra ui instalado e configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,11 @@
-import { useState } from 'react';
-import { useQuery } from 'react-query';
-
-import theme from './styles/theme';
-import { Center, ChakraProvider } from '@chakra-ui/react';
-import { Header } from './components/commons/Header/index';
-import { VerticalCard } from './components/card/VerticalCard';
-import { vertical_cards } from './components-mock.json';
-
-export interface Data {
-  id: number;
-  email: string;
-  first_name: string;
-  last_name: string;
-  avatar: string;
-}
-
-async function getUser(id: number) {
-  const request = await fetch(
-    `https://reqres.in/api/users/${id}?delay=1`,
-  );
-
-  const response = await request.json();
-
-  if (!request.ok) {
-    throw new Error(response.error);
-  }
-  return response.data as Data;
-}
+import { useState } from 'react'
 
 function App() {
-  const [currentUserId, setCurrentUserId] = useState(1);
-  const { data, isError, isLoading, isFetching } = useQuery(
-    ['users', currentUserId],
-    () => getUser(currentUserId),
-    {
-      staleTime: 50000,
-    },
-  );
-
-  if (isError) {
-    return (
-      <section>
-        <p>Something went wrong</p>
-      </section>
-    );
-  }
-
-  if (!data || isLoading) {
-    return (
-      <section>
-        <p>Loading...</p>
-      </section>
-    );
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <ChakraProvider theme={theme}>
-      <div>
-        <Header />
-        <Center>
-          {vertical_cards.map((vertical_card) => (
-            <VerticalCard
-              key={vertical_card.title}
-              card={vertical_card}
-            />
-          ))}
-        </Center>
-      </div>
-      <section>
-        <img src={data.avatar} />
-        <p>
-          {data.first_name} {data.last_name} ({data.id})
-        </p>
-        <p>Email: {data.email}</p>
-        <div>
-          <button
-            onClick={() =>
-              setCurrentUserId((prev) => prev - 1)
-            }
-          >
-            Prev
-          </button>
-          <button
-            onClick={() =>
-              setCurrentUserId((prev) => prev + 1)
-            }
-          >
-            Next
-          </button>
-        </div>
-        {isFetching && (
-          <small>We are updating your data</small>
-        )}
-      </section>
-    </ChakraProvider>
-  );
+    <h1>helooow world</h1>
+  )
 }
-export default App;
+
+export default App
